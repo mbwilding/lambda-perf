ed171192bef8160d9520f48c8a7fd0c11c4ff73f
Matthew Wilding <mbwilding@gmail.com>
2023-10-06 00:16:45 +0800

    prevent cache

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,7 @@ const dataManager = {
 
 const load = async (dataManager) => {
   const request = await fetch(
-    "https://raw.githubusercontent.com/mbwilding/lambda-perf/main/data/last.json?0.13820548599048021"
+    "https://raw.githubusercontent.com/mbwilding/lambda-perf/main/data/last.json?0.8061968626040681"
   );
   const json = await request.json();
   dataManager.fetchData = json;
