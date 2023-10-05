79cfb9aeca015d9158b9a6889b533ae249658dd4
Matthew Wilding <mbwilding@gmail.com>
2023-10-05 18:48:05 +0800

    Changes

--- s3-uploader/runtimes/dotnet6/src/Function.cs
+++ s3-uploader/runtimes/dotnet6/src/Function.cs
@@ -1,13 +1,18 @@
 using Amazon.Lambda.Core;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-namespace LambdaPerf
+
+namespace LambdaPerf;
+
+public class Function
 {
-    public class Function
+    public Function()
     {
-        public object Handler()
-        {
-            return new { statusCode = 200 };
-        }
+        
+    }
+    
+    public object Handler(ILambdaContext context)
+    {
+        return new { statusCode = 200 };
     }
 }
