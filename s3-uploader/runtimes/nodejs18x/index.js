4879d422247f1f2e19cbe28b863683459e477b63
Matthew Wilding <mbwilding@gmail.com>
2023-10-06 14:58:13 +0800

    NodeJS 18

--- s3-uploader/runtimes/nodejs18x/index.js
+++ s3-uploader/runtimes/nodejs18x/index.js
@@ -1,5 +1,28 @@
-exports.handler = () => {
-    return {
-        statusCode: 200
-    };
+exports.handler = async (event, context) => {
+    const aws = require('aws-sdk');
+    const s3 = new aws.S3();
+    
+    const region = process.env.AWS_REGION;
+    if (!region) {
+        throw new Error("AWS_REGION not set");
+    }
+
+    const bucket_name = `lambda-perf-${region}`;
+    const bucket_key = `test/${context.functionName}`;
+
+    try {
+        for (let i = 0; i < 250; i++) {
+            const params = {
+                Bucket: bucket_name,
+                Key: bucket_key,
+                ContentType: 'text/plain',
+                Body: i.toString()
+            };
+            await s3.putObject(params).promise();
+        }
+
+        await s3.deleteObject({ Bucket: bucket_name, Key: bucket_key }).promise();
+    } catch (e) {
+        throw new Error(`An error occurred: ${e.message}`);
+    }
 };
