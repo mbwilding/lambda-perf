6edc5b2a396512999494bb322be89e9d9b95811b
Matthew Wilding <mbwilding@gmail.com>
2023-10-06 15:52:38 +0800

    Test

--- s3-uploader/runtimes/nodejs18x/build.sh
+++ s3-uploader/runtimes/nodejs18x/build.sh
@@ -1,6 +1,9 @@
 DIR_NAME="./runtimes/$1"
 ARCH=$2
 
+pushd ${DIR_NAME}
 yarn install
+popd
+
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
-zip -r ${DIR_NAME}/code_${ARCH}.zip ${DIR_NAME}/index.js ${DIR_NAME}/node_modules
+zip -r ${DIR_NAME}/code_${ARCH}.zip ${DIR_NAME}
