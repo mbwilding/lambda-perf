f9fa8b066ebc8c0775aad8f1a8d20f485125dfba
Matthew Wilding <mbwilding@gmail.com>
2023-10-06 15:27:55 +0800

    Added packages

--- s3-uploader/runtimes/nodejs18x/build.sh
+++ s3-uploader/runtimes/nodejs18x/build.sh
@@ -3,4 +3,4 @@ ARCH=$2
 
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
-zip -j ${DIR_NAME}/code_${ARCH}.zip ${DIR_NAME}/index.js
+zip -r ${DIR_NAME}/code_${ARCH}.zip ${DIR_NAME}/index.js ${DIR_NAME}/node_modules
